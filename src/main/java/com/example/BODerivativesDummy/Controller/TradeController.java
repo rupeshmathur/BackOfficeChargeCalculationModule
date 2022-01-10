b24c603c635ae7ddc0d5ce022a25498b613be360
RupeshMathur <mathurupesh92@gmail.com>
2022-01-10 22:18:15 +0530

    Refactoring

--- src/main/java/com/example/BODerivativesDummy/Controller/TradeController.java
+++ src/main/java/com/example/BODerivativesDummy/Controller/TradeController.java
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BODerivativesDummy.Entities.ChargeRateInstruction;
 import com.example.BODerivativesDummy.Entities.CommissionInstruction;
 import com.example.BODerivativesDummy.Entities.EventRule;
 import com.example.BODerivativesDummy.Entities.Trade;
 import com.example.BODerivativesDummy.POJO.Charge;
 import com.example.BODerivativesDummy.POJO.Commission;
 import com.example.BODerivativesDummy.POJO.Fee;
+import com.example.BODerivativesDummy.Service.ChargeCalculationServiceImpl;
 import com.example.BODerivativesDummy.Service.EventRuleService;
 import com.example.BODerivativesDummy.Service.TradeService;
 
@@ -28,37 +30,18 @@ import com.example.BODerivativesDummy.Service.TradeService;
 public class TradeController {
 
 	TradeService tradeService;
-	EventRuleService eventRuleService;
+	ChargeCalculationServiceImpl chargeCalServiceImpl;
 
 	@Autowired
-	public TradeController(TradeService tradeService, EventRuleService eventRuleService) {
-		this.eventRuleService = eventRuleService;
+	public TradeController(TradeService tradeService, ChargeCalculationServiceImpl chargeCalServiceImpl) {
 		this.tradeService = tradeService;
+		this.chargeCalServiceImpl = chargeCalServiceImpl;
 	}
 
 	@PostMapping("/trade")
 	public List<Charge> insertTrade(@RequestBody Trade trade) {
 		List<Charge> charges = new ArrayList<Charge>();
-		List<EventRule> allRules = (List<EventRule>) eventRuleService.findAllEventRules();
-		List<EventRule> filteredEventRule = allRules.stream()
-				.filter(er -> er.getExchangeName().equals(trade.getExchangeName())
-						&& trade.getTradeDate().compareTo(er.getChargeRateInstruction().getChargeStartDate()) > 0
-						&& trade.getTradeDate().compareTo(er.getChargeRateInstruction().getChargeEndDate()) < 0)
-				.collect(Collectors.toList());
-
-		if (!filteredEventRule.isEmpty()) {
-			for (EventRule er : filteredEventRule) {
-				if (er.getChargeRateInstruction() instanceof CommissionInstruction) {
-					Charge comm = new Commission();
-					Charge calculatedCommission = (Commission) comm.processCharge(trade, er);
-					charges.add(calculatedCommission);
-				} else {
-					Charge fee = new Fee();
-					Charge calculatedFee = (Fee) fee.processCharge(trade, er);
-					charges.add(calculatedFee);
-				}
-			}
-		}
+		charges = chargeCalServiceImpl.calculateCharge(trade, charges);
 		tradeService.saveTrade(trade);
 		if (charges.isEmpty()) {
 			System.out.println("No satisfied rules found for charge calculation");
