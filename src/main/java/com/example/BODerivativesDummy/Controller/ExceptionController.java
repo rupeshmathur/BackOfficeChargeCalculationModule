73ef9f8f3af2e9f14c21c71d961beaf4b3ecedb1
Rupesh Mathur <mathurupesh92@gmail.com>
2022-02-10 17:09:44 +0530

    Single exception controller used for different exception handling

---
+++ src/main/java/com/example/BODerivativesDummy/Controller/ExceptionController.java
@@ -0,0 +1,24 @@
+package com.example.BODerivativesDummy.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.BODerivativesDummy.Exceptions.DateInvalidException;
+import com.example.BODerivativesDummy.Exceptions.QuantityOrPriceException;
+
+@ControllerAdvice
+public class ExceptionController {
+
+	@ExceptionHandler(value = DateInvalidException.class)
+	public ResponseEntity<Object> eventRuleDateException(DateInvalidException dateInvalid) {
+		return new ResponseEntity<>("Charge start date cannot be greater then charge end date", HttpStatus.NOT_FOUND);
+
+	}
+
+	@ExceptionHandler(value = QuantityOrPriceException.class)
+	public ResponseEntity<Object> qtyOrPriceException(QuantityOrPriceException quantityOrPriceException) {
+		return new ResponseEntity<>("Invalid quantity or price", HttpStatus.NOT_FOUND);
+	}
+}
